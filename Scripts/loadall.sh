26c70f2dad25ee56dc47128555847a5d8210b59e
nieyuanyuan <nieyuanyuan@bytedance.com>
2019-07-09 10:57:22 +0800

    minor updates

---
+++ Scripts/loadall.sh
@@ -0,0 +1,8 @@
+source $HOME/Scripts/loadgen.sh
+source $HOME/Scripts/loadinotify.sh
+source $HOME/Scripts/loadiotop.sh
+source $HOME/Scripts/loadmyperf.sh
+source $HOME/Scripts/loadmysql.sh
+source $HOME/Scripts/loadperf.sh
+source $HOME/Scripts/loadpidstat.sh
+source $HOME/Scripts/loadsar.sh
